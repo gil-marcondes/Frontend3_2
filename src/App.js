c022e82aa88ffbef08f2a6e5543d1a539b6b47ad
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-11-28 18:06:11 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import Logar from './Logar';
+
+class App extends Component{
+  render(){
+  return (
+  <div className="App">
+  <h1>Tecnologias Front-end: Atividade3</h1>
+        <Logar/>
+		
+      </div>
+	  );
+}
+}
+
+export default App;
